0e028403272b8c067a8386cb0dce23d0e0a5c47f
willywanady06 <wanadywilly@gmail.com>
2022-08-18 10:43:12 +0700

    aaaa

--- FSAWebSystem/FSAWebSystem/Models/Context/Configuration.cs
+++ FSAWebSystem/FSAWebSystem/Models/Context/Configuration.cs
@@ -9,6 +9,51 @@ namespace FSAWebSystem.Models.Context
     {
         public static async void Initialize(FSAWebSystemDbContext _db, UserManager<FSAWebSystemUser> userManager, IRoleService _roleService)
         {
+            var listWL = new List<WorkLevel>();
+            var wls = new List<string>()
+            {
+                "KAM WL 1",
+                "KAM WL 2",
+                "SOM MT WL 1",
+                "SOM MT WL 2",
+                "CDM WL 3",
+                "VP MTDA",
+                "CCD",
+                "CORE VP",
+            };
+            if(_db.WorkLevels.Any())
+            {
+                var wlInDb = _db.WorkLevels.Where(x => wls.Contains(x.WL)).Select(x => x.WL).ToList();
+                var wlToInsert = wls.Except(wlInDb);
+                foreach(var wl in wlToInsert)
+                {
+                    var worklevel = new WorkLevel();
+                    worklevel.IsActive = true;
+                    worklevel.Id = Guid.NewGuid();
+                    worklevel.WL = wl;
+                    worklevel.CreatedAt = DateTime.Now;
+                    worklevel.CreatedBy = "System";
+                    listWL.Add(worklevel);
+                }
+
+            }
+            else
+            {
+                var workLevels = wls.Select(x => new WorkLevel
+                {
+                    Id = Guid.NewGuid(),
+                    IsActive = true,
+                    WL = x,
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = "System"
+                });
+
+                listWL.AddRange(workLevels);
+            }
+
+            _db.WorkLevels.AddRange(listWL);
+            _db.SaveChanges();
+
             if (!_db.RoleUnilevers.Any())
             {
                 var listRole = new List<RoleUnilever>
